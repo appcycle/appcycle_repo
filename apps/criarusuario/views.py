0f5363dc0ead3d39de87afe02dc70bce0de32e70
gpxlqa <gabrielpeschl@gmail.com>
2021-07-17 17:36:34 -0300

    permissao deletar usuario

--- apps/criarusuario/views.py
+++ apps/criarusuario/views.py
@@ -1,12 +1,13 @@
 from django.contrib.auth import login, authenticate
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
 
 from django.shortcuts import render
 from django.contrib.auth import get_user_model
-from django.views.generic import ListView
+from django.views.generic import ListView, DeleteView
 
 from apps.criarusuario.forms import SignUpForm
 
@@ -33,3 +34,12 @@ class ListUsuariosRegistrados(ListView):
     paginate_by = 10
     model = get_user_model()
     ordering = ['-id']
+
+
+
+#@permission_required('auth.delete_user')
+@method_decorator(login_required, name='dispatch')
+class DeleteUser(DeleteView):
+    model = get_user_model()
+    success_url = reverse_lazy('listausuarios')
+
